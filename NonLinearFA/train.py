d363e041f3b09ead903a3c4fe95a4f2383d77dcb
nishanthvanand <nishanth127127@gmail.com>
2020-03-18 13:07:02 -0400

    added etd for drl

--- NonLinearFA/train.py
+++ NonLinearFA/train.py
@@ -16,6 +16,8 @@ class trainer():
 				td_errors = []
 				c_grad = []
 				beta_list = []
+				emph_list = []
+				F_t = 0
 
 				for data in self.data_list[episode]:
 					c_f, n_f, rew, beta, done = data
@@ -30,6 +32,10 @@ class trainer():
 					td_errors.append(curr_error.detach()[0])
 					c_grad.append(c_val)
 					beta_list.append(beta)
+					if self.args.trace_type == "etd":
+						F_t = self.args.gamma * F_t + self.args.intrst
+						M_t = (1-beta) * self.args.intrst + beta * F_t
+						emph_list.append(M_t)
 
 				if self.args.trace_type == "gated":
 					t_n_bar = td_errors[-1]
@@ -50,6 +56,17 @@ class trainer():
 					for idx, (grad, delta, beta) in enumerate(zip(reversed(c_grad[:-1]), reversed(td_errors[:-1]), reversed(beta_list[:-1]))):
 						t_n_bar = delta + self.args.gamma * (1-nxt_beta) * t_n_bar
 						t_n = grad * t_n_bar
+						gradient -= t_n
+						nxt_beta = beta
+
+				elif self.args.trace_type == "etd":
+					t_n_bar = td_errors[-1]
+					t_n = emph_list[-1] * c_grad[-1] * t_n_bar
+					nxt_beta = beta_list[-1]
+					gradient = -t_n
+					for idx, (grad, delta, beta, M) in enumerate(zip(reversed(c_grad[:-1]), reversed(td_errors[:-1]), reversed(beta_list[:-1]), reversed(emph_list[:-1]))):
+						t_n_bar = delta + self.args.gamma * (1-nxt_beta) * t_n_bar
+						t_n = grad * M * t_n_bar
 						gradient -= t_n
 						nxt_beta = beta
 
